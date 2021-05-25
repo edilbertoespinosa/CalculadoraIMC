aa0d5f1c67dd54b899ccdf3bdf5992cc43bc7756
Edilberto Espinosa <edilbertoespinosa@gmail.com>
2021-05-25 18:13:03 -0500

    Commit Inicial

---
+++ CalculadoraIMC2/CalculadoraIMC2/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraIMC2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if((!string.IsNullOrEmpty(Altura.Text)) && (!string.IsNullOrEmpty(Peso.Text)))
+            { 
+                var altura = double.Parse(Altura.Text);
+                var peso = double.Parse(Peso.Text);
+                var imc = peso / (altura * altura);
+                IMC.Text = imc.ToString();
+
+                string resultado = "";
+                if(imc<18.5)
+                {
+                    resultado = "Tienes bajo peso";
+                }
+                else if((imc>=18.5) && (imc<=25))
+                {
+                    resultado = "Tu peso es normal";
+                }
+                else if((imc>=25) && (imc<30))
+                {
+                    resultado = "Tienes sobrepeso";
+                }
+                else
+                {
+                    resultado = "Tienes obesidad, ¡Cuidate!";
+                }
+                DisplayAlert("Resultado", resultado, "OK");
+            }
+            else
+            {
+                DisplayAlert("Datos Faltantes","Debes proporcionar datos de peso y altura","OK");
+            }
+        }
+    }
+}
